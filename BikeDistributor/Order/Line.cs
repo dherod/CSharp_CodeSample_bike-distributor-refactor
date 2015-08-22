2366aacc40e6e3612a0c90310ad063579cabd928
Don Herod <don.herod@gmail.com>
2015-08-21 22:00:11 -0400

    Move Order and Line to folder for increased readability

---
+++ BikeDistributor/Order/Line.cs
@@ -0,0 +1,36 @@
+﻿namespace BikeDistributor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Line
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bike"></param>
+        /// <param name="quantity"></param>
+        public Line(Bike bike, int quantity)
+        {
+            Bike = bike;
+            Quantity = quantity;
+        }
+
+        #endregion Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Bike Bike { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Quantity { get; private set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ BikeDistributor/Order/Order.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BikeDistributor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Order
+    {
+        #region Private Variables
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private const double _taxRate = .0725d;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IList<Line> _lines = new List<Line>();
+
+        #endregion Private Variables
+
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="company"></param>
+        public Order(string company)
+        {
+            Company = company;
+            TaxRate = _taxRate;
+        }
+
+        #endregion Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Company { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double TaxRate { get; private set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="line"></param>
+        public void AddLine(Line line)
+        {
+            _lines.Add(line);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IList<Line> GetLines()
+        {
+            return _lines;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public string CreateReceipt(Receipt type)
+        {
+            IReceipt receipt = ReceiptSimpleFactory.CreateReceipt(type);
+            return receipt.GenerateReceipt(this);
+        }
+
+        #endregion Public Methods
+    }
+}
